5eb38e55d9f2742b8d640f787a0a1574a115817d
Nathan Rendon <naterendon@gmail.com>
2019-11-09 10:25:59 -0600

    added the new schema with users, carsWant, warsHave tables

--- models/schema.sql
+++ models/schema.sql
@@ -1,7 +1,31 @@
 DROP DATABASE IF EXISTS mygaragedb;
 CREATE DATABASE mygaragedb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE users
+(
+    id int not null auto_increment,
+    users_name varchar(50) not null,
+    primary key (id)
+);
+
+create table carsWant
+(
+    car_year int not null,
+    car_make varchar (50) not null,
+    car_model varchar (50) not null,
+    car_trim varchar (50) not null,
+    primary key (id),
+    foreign key (users_name) references users(id)
+);
+
+create table warsHave
+(
+    car_year int not null,
+    car_make varchar (50) not null,
+    car_model varchar (50) not null,
+    car_trim varchar (50) not null,
+    primary key (id),
+    foreign key (users_name) references users(id)
+);
 
 
