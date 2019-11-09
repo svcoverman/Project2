8e7cfc721d705d2c56706cf5194ff9270cb640b8
Nathan Rendon <naterendon@gmail.com>
2019-11-09 11:52:14 -0600

    added firebase.js with code linking to firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use 
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBG5lLw3YGqEzRAT6GAidRA4IVcd2XrJK4",
+    authDomain: "mygarage-d55d0.firebaseapp.com",
+    databaseURL: "https://mygarage-d55d0.firebaseio.com",
+    projectId: "mygarage-d55d0",
+    storageBucket: "mygarage-d55d0.appspot.com",
+    messagingSenderId: "154974821878",
+    appId: "1:154974821878:web:f0cdb197ae0cbf3781fbd7",
+    measurementId: "G-1R01909MNY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
